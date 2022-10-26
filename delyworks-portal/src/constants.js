aefa7f12571e6a925897c054f3dfc82486fac17b
Daniel Jevremović <daniel.jevremovic@gmail.com>
2022-10-26 19:59:32 +0200

    add colors and URL constants

---
+++ delyworks-portal/src/constants.js
@@ -0,0 +1,7 @@
+export const WOWHEAD_ITEM_URL = 'https://www.wowhead.com/wotlk/item='
+export const WOWHEAD_ICON_URL = 'https://wow.zamimg.com/images/wow/icons/large/'
+export const BLIZZARD_ICON_URL = 'https://render.worldofwarcraft.com/classic-eu/icons/56/'
+
+export const BLIZZARD_API = 'https://eu.api.blizzard.com'
+export const BLIZZARD_API_ITEM = '/data/wow/item/'
+export const BLIZZARD_API_ITEM_MEDIA = '/data/wow/media/item/'
