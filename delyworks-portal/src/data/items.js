b112c862b0b777836cbe5eec5c5d62c711221bea
Daniel Jevremović <daniel.jevremovic@gmail.com>
2022-10-31 21:54:18 +0100

    add cloth items

--- delyworks-portal/src/data/items.js
+++ delyworks-portal/src/data/items.js
@@ -1,7 +1,8 @@
 import ores from '../data/ores.json'
 import herbs from '../data/herbs.json'
 import leather from '../data/leather.json'
+import cloth from '../data/cloth.json'
 
-const items = [...ores, ...herbs, ...leather]
+const items = [...ores, ...herbs, ...leather, ...cloth]
 
 export default items
