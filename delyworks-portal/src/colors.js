7c41e98bfe8b480565a83ddad5a5e0ebf0daba27
Daniel Jevremović <daniel.jevremovic@gmail.com>
2022-11-01 19:33:03 +0100

    add class and power colors

--- delyworks-portal/src/colors.js
+++ delyworks-portal/src/colors.js
@@ -6,3 +6,23 @@ export const qualityColors = {
   epic: '#A335EE',
   legendary: '#FF8000',
 }
+
+export const classColors = {
+  mage: '#3FC7EB',
+  priest: '#FFFFFF',
+  warlock: '#8788EE',
+  rogue: '#FFF468',
+  druid: '#FF7C0A',
+  hunter: '#AAD372',
+  shaman: '#0070DD',
+  warrior: '#C69B6D',
+  paladin: '#F48CBA',
+  deathKnight: '#C41E3A',
+}
+
+export const powerColors = {
+  mana: '#0000FF',
+  rage: '#FF0000',
+  energy: '#FFFF00',
+  runicPower: '#00D1FF',
+}
