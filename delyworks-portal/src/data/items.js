1aeade08af4375c5ca8894c3075019faf3588133
Daniel Jevremović <daniel.jevremovic@gmail.com>
2022-11-01 19:26:15 +0100

    add pigment items

--- delyworks-portal/src/data/items.js
+++ delyworks-portal/src/data/items.js
@@ -1,11 +1,21 @@
 import ores from '../data/ores.json'
 import herbs from '../data/herbs.json'
 import leather from '../data/leather.json'
-import cloth from '../data/cloth.json'
+import cloths from '../data/cloths.json'
 import enchanting from '../data/enchanting.json'
-import elemental from '../data/elemental.json'
+import elementals from '../data/elementals.json'
 import other from '../data/other.json'
+import pigments from '../data/pigments.json'
 
-const items = [...ores, ...herbs, ...leather, ...cloth, ...enchanting, ...elemental, ...other]
+const items = [
+  ...ores,
+  ...herbs,
+  ...leather,
+  ...cloths,
+  ...enchanting,
+  ...elementals,
+  ...other,
+  ...pigments,
+]
 
 export default items
