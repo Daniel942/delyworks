b78281e07f7c1b0500c75ca6cf8463378e0a7030
Daniel Jevremović <daniel.jevremovic@gmail.com>
2022-11-01 18:56:39 +0100

    add elemental items

--- delyworks-portal/src/data/items.js
+++ delyworks-portal/src/data/items.js
@@ -3,7 +3,8 @@ import herbs from '../data/herbs.json'
 import leather from '../data/leather.json'
 import cloth from '../data/cloth.json'
 import enchanting from '../data/enchanting.json'
+import elemental from '../data/elemental.json'
 
-const items = [...ores, ...herbs, ...leather, ...cloth, ...enchanting]
+const items = [...ores, ...herbs, ...leather, ...cloth, ...enchanting, ...elemental]
 
 export default items
