ecec42de5641d807f4824891a93e74c95721469e
Daniel Jevremović <daniel.jevremovic@gmail.com>
2022-11-01 19:13:41 +0100

    add other items

--- delyworks-portal/src/data/items.js
+++ delyworks-portal/src/data/items.js
@@ -4,7 +4,8 @@ import leather from '../data/leather.json'
 import cloth from '../data/cloth.json'
 import enchanting from '../data/enchanting.json'
 import elemental from '../data/elemental.json'
+import other from '../data/other.json'
 
-const items = [...ores, ...herbs, ...leather, ...cloth, ...enchanting, ...elemental]
+const items = [...ores, ...herbs, ...leather, ...cloth, ...enchanting, ...elemental, ...other]
 
 export default items
